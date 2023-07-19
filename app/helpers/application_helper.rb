c01c6946845d778679f118751ed82a1ef2005ac7
cristianzaninovic <czaninovic@gmail.com>
2023-07-19 00:25:56 -0400

    views customize

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,7 @@ module ApplicationHelper
   def menu_and_title
     content_tag(:header, class: 'bg-dark text-white text-center py-5') do
       content_tag(:div, class: 'container') do
-        content_tag(:div, class: 'row align-items-center') do
+        content_tag(:div, class: 'row align-items-left') do
           concat(content_tag(:div, class: 'col-lg-6 text-lg-start') do
             content_tag(:h1, 'Cristian Zaninovic', class: 'site-title text-white')
           end)
